1f714090da145142c094729de80223e52b2045d7
hbt233 <75663841+hbt233@users.noreply.github.com>
2023-06-09 01:46:14 +0800

    Add files via upload

---
+++ dorm_front/assets/tableHandle-8f8d535a.js
@@ -0,0 +1,1 @@
+import{e as t}from"./index-f29a31d7.js";function i(n,o){const a=t(10),c=t(1),r=t(0),s=e=>{a.value=e,n()},l=e=>{c.value=e,n()};function u(){Object.keys(o).forEach(e=>{o[e]=""}),n()}return{pageSize:a,pageNum:c,total:r,handleCurrentChange:l,handleSizeChange:s,reset:u}}export{i as t};
